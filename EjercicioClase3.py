ab2d7a8f6b61b6b9cd1fb4efabd3d09e498e6601
LucasGimenezKramer <159184152+LucasGimenezKramer@users.noreply.github.com>
2024-02-15 19:52:45 -0300

    Add files via upload

--- EjercicioClase3.py
+++ EjercicioClase3.py
@@ -1,16 +1,20 @@
-nro = 1
+nro1 = int (input("Ingrese el primer numero: "))
+nro2 = int (input("Ingrese el segundo numero: "))
+opcion = int (input("Ingrese el numero 1 para sumar o el numero 2 para restar: "))
 
 def sumar(num1,num2):
     suma = num1 + num2
     return suma 
 
 def restar(num1,num2):
-    resta = num1 + num2
+    resta = num1 - num2
     return resta 
 
-if nro == 1:
-    print (sumar(5,3))
-elif nro == 2:
-    print (restar(5,3))
+if opcion == 1:
+    resultado_suma = sumar(nro1,nro2)
+    print (f'La suma entre {nro1} y {nro2} es: {resultado_suma}')
+elif opcion == 2:
+    resultado_resta = restar(nro1,nro2)
+    print (f'La resta entre {nro1} y {nro2} es: {resultado_resta}')
 else:
     print ("Numero incorrecto")
