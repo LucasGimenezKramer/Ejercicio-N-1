6ec969d2a4e8351b448c931dcd2ecbbbcc3ac920
LucasGimenezKramer <159184152+LucasGimenezKramer@users.noreply.github.com>
2024-02-15 16:53:02 -0300

    Add files via upload

---
+++ EjercicioClase3.py
@@ -0,0 +1,16 @@
+nro = 1
+
+def sumar(num1,num2):
+    suma = num1 + num2
+    return suma 
+
+def restar(num1,num2):
+    resta = num1 + num2
+    return resta 
+
+if nro == 1:
+    print (sumar(5,3))
+elif nro == 2:
+    print (restar(5,3))
+else:
+    print ("Numero incorrecto")
